7b9dec8f401972d0f91487ba5f0ca5d22db4d439
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2023-08-30 19:07:40 +0000

    Cycle button added

--- src/js/component/traffic.jsx
+++ src/js/component/traffic.jsx
@@ -4,6 +4,46 @@ import React, { useState } from 'react';
 
 function Traffic () {
 
+    function cycle() {
+        let count = 0
+        const interval = setInterval(() => {
+            switch (count) {
+                case 0 : 
+                console.log("red");
+                setHighlightRed(true);
+                setHighlightYellow(false);
+                setHighlightGreen(false);
+                count ++
+                break
+                case 1 : 
+                console.log("yellow")
+                setHighlightRed(false);
+                setHighlightYellow(true);
+                setHighlightGreen(false);
+                count ++
+                break
+                case 2 : 
+                console.log("green")
+                setHighlightRed(false);
+                setHighlightYellow(false);
+                setHighlightGreen(true);
+                count ++
+                break
+                case 3 : 
+                console.log("all off")
+                setHighlightRed(false);
+                setHighlightYellow(false);
+                setHighlightGreen(false);
+                clearInterval(interval)
+                break
+            }
+
+          }, 1000);
+    }
+
+
+    
+
     const [highlightRed, setHighlightRed] = useState(false)
     const [highlightYellow, setHighlightYellow] = useState(false)
     const [highlightGreen, setHighlightGreen] = useState(false)
@@ -13,8 +53,8 @@ function Traffic () {
             <div className={`${highlightRed ? "highlight" : ""}` + " circle bg-danger mx-auto"} onClick={() => {setHighlightRed(!highlightRed); setHighlightYellow(false); setHighlightGreen(false)}}></div>
             <div className={`${highlightYellow ? "highlight" : ""}` +  " circle bg-warning mx-auto"} onClick={() => {setHighlightYellow(!highlightYellow); setHighlightRed(false); setHighlightGreen(false) }}></div>
             <div className={`${highlightGreen ? "highlight" : ""}` +  " circle bg-success mx-auto"} onClick={() => {setHighlightGreen(!highlightGreen); setHighlightRed(false); setHighlightYellow(false)}}></div>
+            <button onClick={cycle}>Cycle</button>
         </div>
     )
 }
-// "circle bg-danger mx-auto"
 export default Traffic
